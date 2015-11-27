0254941764b091fc15dcd73096ceebaefb8d9947
refond <refon@pobox.com>
2015-11-27 19:35:48 +0100

    Successfully use psmod1 Module! Possible routing clash must still be investigated further.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,13 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-class Application extends Controller {
+import javax.inject.Inject
+import com.escalesoft.psmod1.Psmod1Component
+
+class Application @Inject() (psmod1: Psmod1Component) extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Message from psmaster1"))
+    Ok(views.html.index(psmod1.wave("Message from psmaster1")))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -4,9 +4,10 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
+  "com.escalesoft" %% "psmod1" % "1.0-SNAPSHOT",
   jdbc,
   cache,
   ws,
